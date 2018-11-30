03e8b3ffdaf09317075daaf8cae7c2227b52bd6d
carolinezhao <chaohsimeng@gmail.com>
2018-11-30 23:57:08 +0800

    thunk action creator

--- reddit-api/README.md
+++ reddit-api/README.md
@@ -6,3 +6,9 @@
 - 通知 reducer 请求失败
   
 通常定义 3 个 ActionType：*_REQUEST, *_SUCCESS, *_FAILURE
+
+异步 Action 创建函数
+- 通过使用指定的 middleware，action 创建函数除了返回 action 对象外还可以返回函数。
+- 这个函数会被 Redux Thunk middleware 执行。
+- 这个函数并不需要保持纯净；它还可以带有副作用，包括执行异步 API 请求。
+- 这个函数还可以 dispatch action，就像 dispatch 前面定义的同步 action 一样。
--- reddit-api/src/actions.js
+++ reddit-api/src/actions.js
@@ -1,6 +1,8 @@
 import fetch from 'cross-fetch'
 
-// 把请求事件和 UI 事件分离
+// 把请求事件和 UI 事件分离很重要
+// 随着应用变得复杂，有些用户操作（比如，预加载最流行的 subreddit，或者一段时间后自动刷新过期数据）后需要马上请求数据。路由变化时也可能需要请求数据。
+
 // 由网络请求控制
 export const REQUEST_POSTS = 'REQUEST_POSTS'
 export const RECEIVE_POSTS = 'RECEIVE_POSTS'
@@ -38,12 +40,13 @@ function receivePosts(subreddit, json) {
   }
 }
 
+// thunk action creator
 function fetchPosts(subreddit) {
-  return dispatch => {
-    dispatch(requestPosts(subreddit))
-    return fetch(`https://www.reddit.com/r/${subreddit}.json`)
+  return dispatch => { // dispatch 方法作为参数传入
+    dispatch(requestPosts(subreddit)) // 更新应用的 state 来通知 API 请求发起了
+    return fetch(`https://www.reddit.com/r/${subreddit}.json`) // 返回值是一个 Promise
       .then(response => response.json())
-      .then(json => dispatch(receivePosts(subreddit, json)))
+      .then(json => dispatch(receivePosts(subreddit, json))) // 使用 API 请求结果来更新应用的 state
   }
 }
 
